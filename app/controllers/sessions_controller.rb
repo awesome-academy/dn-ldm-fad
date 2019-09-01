ba1ee4aaec13d7a998179c1cfc82c9c2412f1b57
Le Duc Manh <le.duc.manh@sun-asterisk.com>
2019-09-01 22:33:01 +0700

    Login, logout user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  before_action :load_user, only: :create
+  before_action :check_logged_in, only: :new
+
+  def new; end
+
+  def create
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      redirect_to root_url
+    else
+      flash.now[:danger] = t "users.login_fail"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by email: params[:session][:email].downcase
+    return if @user
+    flash[:warning] = t "users.user_not_found"
+    redirect_to login_path
+  end
+end
